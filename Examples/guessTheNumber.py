cd9379053c79ca042e3e06a20d0ea907ccb40804
Jorge Silva Developer MEX <101852593+JorgeDeveloperMEX@users.noreply.github.com>
2024-07-29 21:32:06 -0600

    JS - adding file guessTheNumber

---
+++ Examples/guessTheNumber.py
@@ -0,0 +1,23 @@
+from random import randint
+
+attempts = 0
+getNumber = 0
+secret_number = randint(1,100)
+name = input("Dime tu nombre: ")
+
+print(f"Hi {name}, i just thinking in a number between 1 and 100\nYou can try 8 times and guess the number")
+
+while attempts < 8:
+    getNumber = int(input("Whats the number?: "))
+    attempts += 1
+
+    if getNumber < secret_number:
+        print("My number is higher than estimated")
+    elif getNumber > secret_number:
+        print("MMy number is lower than estimated")
+    else:
+        print(f"Congratulations {name}! You have guessed in {attempts} attempts")
+        break
+
+if getNumber != secret_number:
+    print(f"Sorry, attempts have been exhausted. The secret number was {secret_number}")
